3e2a274f698e83ef04e199fa09315e2c4adda897
ujstor <astipan@gmail.com>
2023-04-24 19:00:20 +0200

    item in db and index.html

--- core/views.py
+++ core/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
+from item.models import Category, Item
 
 
 def index(request):
-    return render(request, 'core/index.html')
+    item = Item.objects.filter(is_sold=False)[0:6]
+    category = Category.objects.all()
+    return render(request, 'core/index.html', {
+        'items': item,
+        'categories': category,
+    })
 
 def contact(request):
     return render(request, 'core/contact.html')
