01a1faa06b37d8bfbd7b1feae1c5b80f08b5e02b
ujstor <astipan@gmail.com>
2023-04-25 21:11:54 +0200

    signup and login

--- core/views.py
+++ core/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from item.models import Category, Item
-
+from .forms import SignupForm, LoginForm
 
 def index(request):
     item = Item.objects.filter(is_sold=False)[0:6]
@@ -12,3 +12,19 @@ def index(request):
 
 def contact(request):
     return render(request, 'core/contact.html')
+
+def terms(request):
+    return render(request, 'core/terms.html')
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/login/')
+    else:
+        form = SignupForm()
+
+    return render(request, 'core/signup.html', {
+        'form': form,
+    })
